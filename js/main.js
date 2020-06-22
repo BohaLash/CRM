57f5c540d9fd4c3b899d40e7fccc1ff041fea62f
BohaLash <dogdanlashko@gmail.com>
2020-06-22 14:16:22 +0300

    😁woohooo!

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// function showFilters() {
+// 	var f = document.getElementByClassName("filters")
+// 	if (f.style.display === "none") {
+// 		f.style.display = "block";
+// 	} else {
+// 		f.style.display = "none";
+// 	}
+// }
+
+// function theme() {
+// 	$('button, section, input').css({
+// 		"box-shadow":"0 0 30px #000",
+//   		"background":"#171717"
+//   	})
+// 	$('body').css({
+//   		"background":"#111",
+//   		"color":"#fff"
+//   	})
+// 	$('svg').css({
+//   		"fill":"#fff"
+//   	})
+  	
+// }
