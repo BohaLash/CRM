eb35d4e932875935a16b8ce0059fade1dc6e1b74
BohaLash <dogdanlashko@gmail.com>
2020-06-24 23:17:24 +0300

    The Best CRM Ever

--- js/main.js
+++ js/main.js
@@ -1,31 +1,5 @@
 // The Best CRM Ever
 
-function toggleTheme() {
-    if (document.body.className == 'th1') {
-        document.body.className = 'th2';
-    } else {
-        document.body.className = 'th1';
-    }
-}
-
-function toggleFilters() {
-    if (document.getElementsByClassName('filters')[0].style.display == 'none') {
-        document.getElementsByClassName('filters')[0].style.display = 'block';
-        document.getElementsByClassName('resp')[0].style.width = 'calc(100% - 100px)';
-        document.getElementsByClassName('resp')[1].style.width = '50px';
-        document.getElementsByClassName('resp')[1].style.paddingTop = '14px';
-    } else {
-        document.getElementsByClassName('filters')[0].style.display = 'none';
-        document.getElementsByClassName('resp')[0].style.width = '';
-        document.getElementsByClassName('resp')[1].style.width = '';
-        document.getElementsByClassName('resp')[1].style.paddingTop = '';
-    }
-}
-
-document.getElementById('theme').onclick = toggleTheme;
-document.getElementsByClassName('filters')[0].style.display = 'none';
-document.getElementById('filters').onclick = toggleFilters;
-
 class CardMeneger {
     constructor(table) {
         this.opened = false
