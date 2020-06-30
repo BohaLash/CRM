b58f632ae061afd60ed25fca3361d917b158649c
BohaLash <dogdanlashko@gmail.com>
2020-06-30 13:32:51 +0300

    The Best CRM Ever

--- js/main.js
+++ js/main.js
@@ -5,17 +5,17 @@ class CardMeneger {
         this.opened = false
         table.find("tr").click((e) => this.rowClicked(e));
         $("body").click((e) => this.close());
-        $(".profile").click(e => e.stopPropagation())
+        $(".card").click(e => e.stopPropagation())
     }
     rowClicked(e) {
         if (this.opened == false) {
-            $('.profile').css("display", "block")
+            $('.card').css("display", "block")
             this.opened = true;
             e.stopPropagation()
         }
     }
     close() {
-        $('.profile').css("display", "none")
+        $('.card').css("display", "none")
         this.opened = false;
     }
 }
